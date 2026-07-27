fc2bba0a04b15a74df46e277c35fe2111ee20291
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-27 12:41:02 +0200

    feat(terraform): use remote state and explicit secure cluster inputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,11 @@
-# Pas de version_prefix codé en dur : DigitalOcean retire régulièrement les
-# anciennes versions (EOL), ce qui casse un prefix figé. On laisse le data
-# source renvoyer la dernière version supportée du moment.
-data "digitalocean_kubernetes_versions" "current" {}
-
 resource "digitalocean_kubernetes_cluster" "opswarden" {
   name    = var.cluster_name
   region  = var.region
-  version = data.digitalocean_kubernetes_versions.current.latest_version
+  version = var.kubernetes_version
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-4gb" # 2 vCPU, 4 Go RAM par noeud
-    node_count = 2
+    size       = var.node_size
+    node_count = var.node_count
   }
 }
